0d09bf7c60fb851be066fcff274f59f931c91640
António Silva <100034891+antoniosilva23@users.noreply.github.com>
2022-07-03 21:11:33 +0100

    arrays

---
+++ cursojs/aula15/ambiente.js
@@ -0,0 +1,17 @@
+let num = [5, 8, 2, 9, 3]
+num.sort()
+// coloca por ordem. mas só pode ser colocado no fim
+//cada vez que for adicionado um valor ele perde a ordem.
+//coloca os valores por ordem crescente, sempre que adicionado.
+
+//num[5] = 6
+// chave 5 recebe valor 6
+
+//num.push(7)
+// atribuir valor 7 a uma chave qualquer
+
+//num.length: qual o tamanho 
+
+console.log(num)
+console.log(`O vetor tem ${num.length} posições`)
+console.log(`O primeiro valor do vetor é ${num[0]}`)
