0271f2e67c3bf13bd3e54ac1be9bba5861132be2
António Silva <100034891+antoniosilva23@users.noreply.github.com>
2022-07-09 14:49:53 +0100

    indexreact

---
+++ reactjs/index.js
@@ -0,0 +1,7 @@
+ReactDOM.render(<h1>Hello, everyone!</h1>, document.getElementById(`root`))
+ReactDOM.render(<h1>Hi, my name is Bob!</h1>, document.querySelector(`.parag`))
+
+ReactDOM.render(<ul><li>Apple</li> <li>Orange</li> <li>Strawberry</li></ul>, 
+document.querySelector(`.list`))
+
+ReactDOM.render(<h1>I´m Learning React!</h1>, document.querySelector(`.MainContent`))
